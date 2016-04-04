3b950f9f644cf58a7ee5946c5fb1b85ab6e94cb9
Daniel Kłódka <danielklodka@gmail.com>
2016-04-04 16:28:23 +0200

    Created a new function to print and evaluate strings

--- ChurchNumerals/liczebniki.py
+++ ChurchNumerals/liczebniki.py
@@ -14,6 +14,10 @@ arg2 = 1
 
 getNum = lambda f: f(f1)(arg1) # f = f f1 arg1
 
+def printAndEval(x):
+	print x, ' = ', eval(x)
+	return
+
 def getChurch (x):
 	if x==0:
 		return zero
@@ -21,38 +25,38 @@ def getChurch (x):
 		return succ(getChurch(x-1))
 
 print  "\nWykorzystanie jako argumentow funkcji f1 i zmiennej arg1:\n"
-print  '(zero)(f1)(arg1) = ', (zero)(f1)(arg1)
-print  '(succ(zero))(f1)(arg1) =', (succ(zero))(f1)(arg1)
-print  '(succ(succ(zero)))(f1)(arg1) =', (succ(succ(zero)))(f1)(arg1)
+printAndEval('(zero)(f1)(arg1)')
+printAndEval('(succ(zero))(f1)(arg1)')
+printAndEval('(succ(succ(zero)))(f1)(arg1)')
 print  '\nDodanie dwoch liczb churcha 50 i 23 da ten sam wynik co liczba churcha 73:'
-print  '(add(getChurch(50))(getChurch(23)))(f1)(arg1) =', (add(getChurch(50))(getChurch(23)))(f1)(arg1)
-print  '(getChurch(73))(f1)(arg1) =', (getChurch(73))(f1)(arg1)
+printAndEval('(add(getChurch(50))(getChurch(23)))(f1)(arg1)')
+printAndEval('(getChurch(73))(f1)(arg1)')
 print  '\nMnozenie dwoch liczb churcha 4 i 3 da ten sam wynik co liczba churcha 12:'
-print  '(mult(getChurch(4))(getChurch(3)))(f1)(arg1) =', (mult(getChurch(4))(getChurch(3)))(f1)(arg1)
-print  '(getChurch(12))(f1)(arg1) =', (getChurch(12))(f1)(arg1)
+printAndEval('(mult(getChurch(4))(getChurch(3)))(f1)(arg1)')
+printAndEval('(getChurch(12))(f1)(arg1)')
 print  '\nPotegowanie dwoch liczb churcha 2 i 5 da ten sam wynik co liczba churcha 32:'
-print  '(exp(getChurch(2))(getChurch(5)))(f1)(arg1) =', (exp(getChurch(2))(getChurch(5)))(f1)(arg1)
-print  '(getChurch(32))(f1)(arg1) =', (getChurch(32))(f1)(arg1)
+printAndEval('(exp(getChurch(2))(getChurch(5)))(f1)(arg1)')
+printAndEval('(getChurch(32))(f1)(arg1)')
 
 print  "\nTeraz zmienimy nasza funkcje oraz argument."
 
 print  "\nWykorzystanie jako argumentow funkcji f2 i zmiennej arg2:\n"
-print  '(zero)(f2)(arg2) = ', (zero)(f2)(arg2)
-print  '(succ(zero))(f2)(arg2) =', (succ(zero))(f2)(arg2)
-print  '(succ(succ(zero)))(f2)(arg2) =', (succ(succ(zero)))(f2)(arg2)
+printAndEval('(zero)(f2)(arg2)')
+printAndEval('(succ(zero))(f2)(arg2)')
+printAndEval('(succ(succ(zero)))(f2)(arg2)')
 print  '\nDodanie dwoch liczb churcha 3 i 8 da ten sam wynik co liczba churcha 11:'
-print  '(add(getChurch(3))(getChurch(8)))(f2)(arg2) =', (add(getChurch(3))(getChurch(8)))(f2)(arg2)
-print  '(getChurch(11))(f2)(arg2) =', (getChurch(11))(f2)(arg2)
+printAndEval('(add(getChurch(3))(getChurch(8)))(f2)(arg2)')
+printAndEval('(getChurch(11))(f2)(arg2)')
 print  '\nMnozenie dwoch liczb churcha 4 i 3 da ten sam wynik co liczba churcha 12:'
-print  '(mult(getChurch(4))(getChurch(3)))(f2)(arg2) =', (mult(getChurch(4))(getChurch(3)))(f2)(arg2)
-print  '(getChurch(12))(f2)(arg2) =', (getChurch(12))(f2)(arg2)
+printAndEval('(mult(getChurch(4))(getChurch(3)))(f2)(arg2)')
+printAndEval('(getChurch(12))(f2)(arg2)')
 print  '\nPotegowanie dwoch liczb churcha 2 i 5 da ten sam wynik co liczba churcha 32:'
-print  '(exp(getChurch(3))(getChurch(2)))(f2)(arg2) =', (exp(getChurch(3))(getChurch(2)))(f2)(arg2)
-print  '(getChurch(9))(f2)(arg2) =', (getChurch(9))(f2)(arg2)
+printAndEval('(exp(getChurch(3))(getChurch(2)))(f2)(arg2)')
+printAndEval('(getChurch(9))(f2)(arg2)')
 
 print  "\nDla ulatwienia mozna zaimplementowac sobie"
 print  "funkcje taka jak getNum = lambda f: f(f1)(arg1)\n"
-print  'getNum(zero) = ', getNum(zero)
-print  'getNum(add(getChurch(35))(getChurch(23))) =', getNum(add(getChurch(35))(getChurch(23)))
-print  'getNum(mult(getChurch(5))(getChurch(5))) =', getNum(mult(getChurch(5))(getChurch(5)))
+printAndEval('getNum(zero)')
+printAndEval('getNum(add(getChurch(35))(getChurch(23)))')
+printAndEval('getNum(mult(getChurch(5))(getChurch(5)))')
 
